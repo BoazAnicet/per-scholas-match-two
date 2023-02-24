9cb49d235fc1dbb37fdf56ce7612f21d34cc2105
Boaz <boazanicet@ymail.com>
2023-02-23 21:05:29 -0500

    Randomized cards

--- main.js
+++ main.js
@@ -3,68 +3,6 @@ const board = document.getElementById("board");
 let arr = [];
 let score = 0;
 
-// resetCards = () => {
-//   arr = [];
-// };
-
-const addToArr = (item) => {
-  console.log("Clicked item", arr[item]);
-  if (arr.length === 0) {
-    arr.push(item);
-  } else if (arr.length === 1) {
-    arr.push(item);
-    if (arr[0] === arr[1]) {
-      // Keep cards from flipping
-      alert("Correct");
-      resetCards();
-      score++;
-    } else {
-      alert("incorrect");
-      resetCards();
-    }
-  }
-  console.log(arr);
-};
-
-// const cards = document.querySelectorAll(".flip-card");
-
-// cards.forEach((card) =>
-//   card.addEventListener("click", () => {
-//     // alert("hello");
-//     // card.classList.add("rotate");
-//     card.children[0].classList.add("rotate");
-//   })
-// );
-
-const stuff = (iter) => {
-  cards[iter].children[0].classList.add("rotate");
-  console.log(cards);
-
-  if (arr.length === 0) {
-    arr.push(cardArr[iter]);
-    // clickedCards.push(cards[iter]) //
-  } else if (arr.length === 1) {
-    arr.push(cardArr[iter]);
-    if (arr[0] === arr[1]) {
-      // Keep cards from flipping
-      resetCards();
-      score++;
-      console.log("Correct");
-    } else {
-      console.log("Incorrect");
-      resetCards();
-    }
-  }
-};
-
-// for (let i = 0; i < cardArr.length; i++) {
-//   let card = Object.assign(document.createElement("button"), {
-//     onclick: () => addToArr(cardArr[i]),
-//     innerHTML: cardArr[i],
-//   });
-//   board.append(card);
-// }
-
 const createCard = (index, value) => {
   // Outer container
   const flipCard = Object.assign(document.createElement("div"), {
@@ -83,7 +21,8 @@ const createCard = (index, value) => {
   // Back
   const flipCardBack = Object.assign(document.createElement("div"), {
     className: "flip-card-back",
-    innerHTML: "B",
+    innerHTML: value,
+    // innerHTML: "B",
   });
   // Append front and back
   flipCardInner.append(flipCardFront);
@@ -94,13 +33,14 @@ const createCard = (index, value) => {
   return flipCard;
 };
 // Card values
-const cardsArray = [1, 1, 2, 2, 3, 3];
+const cardsArray = [1, 1, 2, 2, 3, 3, 4, 4];
+const preShuffledCards = cardsArray.sort((a, b) => 0.5 - Math.random());
 
-for (let i = 0; i < 6; i++) {
+for (let i = 0; i < cardsArray.length; i++) {
   board.append(createCard(i, cardsArray[i]));
 }
 
-const cards = document.querySelectorAll(".flip-card");
+const cardsList = document.querySelectorAll(".flip-card");
 
 // Array of correct cards flipped.
 let correctCards = [];
@@ -108,9 +48,9 @@ let correctCards = [];
 let clickedCards = [];
 
 const rotate = (card, value) => {
-  cards[card].children[0].classList.add("rotate");
+  cardsList[card].children[0].classList.add("rotate");
+  cardsList[card].onclick = () => {};
   clickedCards.push({ card, value });
-  // console.log(cards, "cards");
   console.log(clickedCards);
   if (clickedCards.length == 2) {
     if (clickedCards[0].value === clickedCards[1].value) {
@@ -125,9 +65,11 @@ const rotate = (card, value) => {
 const resetCards = () => {
   setTimeout(() => {
     for (let i = 0; i < clickedCards.length; i++) {
-      cards[clickedCards[i].card].children[0].classList.remove("rotate");
+      cardsList[clickedCards[i].card].children[0].classList.remove("rotate");
+      let card = clickedCards[i].card;
+      let value = clickedCards[i].value;
+      cardsList[clickedCards[i].card].onclick = () => rotate(card, value);
     }
     clickedCards = [];
   }, 500);
-  console.log(clickedCards);
 };
