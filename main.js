dabacc452518340ac47961d43db48f699c44fe37
Boaz <boazanicet@ymail.com>
2023-02-23 22:08:11 -0500

    Added card backs

--- main.js
+++ main.js
@@ -3,6 +3,17 @@ const board = document.getElementById("board");
 let arr = [];
 let score = 0;
 
+let canClickCards = true;
+
+const cardBacks = [
+  "/images/card-backs/endless-constellation.svg",
+  "/images/card-backs/liquid-cheese.svg",
+  "/images/card-backs/radiant-gradient.svg",
+  "/images/card-backs/quilted.svg",
+  "/images/card-backs/hollowed-boxes.svg",
+  "/images/card-backs/slanted-gradient.svg",
+];
+
 const createCard = (index, value) => {
   // Outer container
   const flipCard = Object.assign(document.createElement("div"), {
@@ -16,7 +27,8 @@ const createCard = (index, value) => {
   // Front
   const flipCardFront = Object.assign(document.createElement("div"), {
     className: "flip-card-front",
-    innerHTML: "F",
+    // innerHTML: "F",
+    style: `background-image: url(${cardBacks[5]})`,
   });
   // Back
   const flipCardBack = Object.assign(document.createElement("div"), {
@@ -33,8 +45,19 @@ const createCard = (index, value) => {
   return flipCard;
 };
 // Card values
-const cardsArray = [1, 1, 2, 2, 3, 3, 4, 4];
-const preShuffledCards = cardsArray.sort((a, b) => 0.5 - Math.random());
+let cardsArray = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9];
+
+// The Fisher-Yates algorithm
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+};
+
+shuffleArray(cardsArray);
 
 for (let i = 0; i < cardsArray.length; i++) {
   board.append(createCard(i, cardsArray[i]));
@@ -43,7 +66,7 @@ for (let i = 0; i < cardsArray.length; i++) {
 const cardsList = document.querySelectorAll(".flip-card");
 
 // Array of correct cards flipped.
-let correctCards = [];
+// let correctCards = [];
 //
 let clickedCards = [];
 
